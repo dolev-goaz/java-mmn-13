e9f05af206ea268085de1b0b24e14ec80f403326
dolev goaz <www.dolev@gmail.com>
2024-04-11 20:26:08 +0300

    circle radius is adjustable

--- src/q2/GameController.java
+++ src/q2/GameController.java
@@ -2,6 +2,8 @@ package q2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.layout.*;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
@@ -11,7 +13,7 @@ public class GameController {
     private static final int COLUMN_COUNT = 7;
     private static final int ROW_COUNT = 8;
     private static final int SQUARE_SIZE = 50;
-    private static final double DISC_RADIUS = SQUARE_SIZE / 2.0;
+    private static final double DISC_RADIUS = SQUARE_SIZE / 2.25;
     private static final double SQUARE_CENTER = SQUARE_SIZE / 2.0;
 
     private FourInARow game;
@@ -45,12 +47,14 @@ public class GameController {
         for (int i = 0; i < COLUMN_COUNT; i++) {
             ColumnConstraints constraints = new ColumnConstraints();
             constraints.setPercentWidth(100.0 / COLUMN_COUNT);
+            constraints.setHalignment(HPos.CENTER);
             gameGrid.getColumnConstraints().add(constraints);
         }
         // +1 for button row
         for (int i = 0; i < ROW_COUNT; i++) {
             RowConstraints row = new RowConstraints();
             row.setPercentHeight(100.0 / (ROW_COUNT + 1));
+            row.setValignment(VPos.CENTER);
             gameGrid.getRowConstraints().add(row);
         }
     }
@@ -69,7 +73,7 @@ public class GameController {
 
     private void onButtonPress(Button source) {
         int currentTurn = this.game.getCurrentTurn();
-        
+
         int columnIndex = Integer.parseInt(source.getText()) - 1;
         int rowIndex = this.game.play(columnIndex);
         if (rowIndex == -1) return;
@@ -84,7 +88,6 @@ public class GameController {
                         ? Color.BLUE
                         : Color.RED;
 
-        // TODO: figure out how to center-align this circle to allow smaller radius
         Circle circle = new Circle(SQUARE_CENTER, SQUARE_CENTER, DISC_RADIUS, currentColor);
         this.gameGrid.add(circle, colIndex, rowIndex);
     }
