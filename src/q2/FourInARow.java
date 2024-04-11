cc63709d0e114fe4c9bb53d6516ef35b1afa2067
dolev goaz <www.dolev@gmail.com>
2024-04-11 20:14:01 +0300

    basic gameplay

---
+++ src/q2/FourInARow.java
@@ -0,0 +1,63 @@
+package q2;
+
+public class FourInARow {
+    private static final int EMPTY = -1;
+    public static final int PLAYER_ONE = 1;
+    public static final int PLAYER_TWO = 2;
+
+    private final int width;
+    private final int height;
+
+    private int currentTurn;
+
+    private final int[][] plays;
+
+    public FourInARow(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.currentTurn = PLAYER_ONE;
+
+        this.plays = new int[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.plays[i][j] = EMPTY;
+            }
+        }
+    }
+
+    public int play(int columnIndex) {
+        if (columnIndex < 0 || columnIndex > this.width) {
+            throw new IllegalArgumentException("Invalid column " + columnIndex);
+        }
+
+        int[] column = this.plays[columnIndex];
+        if (column[this.height - 1] != EMPTY) {
+            return -1;
+        }
+
+        // an assignment for the compiler, no real need to assign here since the loop would always find the appropriate row
+        int playedRow = -1;
+        for (int i = 0; i < this.height; i++) {
+            if (column[i] == EMPTY) {
+                playedRow = i;
+                break;
+            }
+        }
+
+        column[playedRow] = this.getCurrentTurn();
+        switchPlayer();
+        return playedRow;
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    private void switchPlayer() {
+        this.currentTurn =
+                this.currentTurn == PLAYER_ONE
+                        ? PLAYER_TWO
+                        : PLAYER_ONE;
+    }
+}
--- src/q2/GameController.java
+++ src/q2/GameController.java
@@ -3,11 +3,17 @@ package q2;
 import javafx.fxml.FXML;
 import javafx.scene.layout.*;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class GameController {
     private static final int COLUMN_COUNT = 7;
     private static final int ROW_COUNT = 8;
     private static final int SQUARE_SIZE = 50;
+    private static final double DISC_RADIUS = SQUARE_SIZE / 2.0;
+    private static final double SQUARE_CENTER = SQUARE_SIZE / 2.0;
+
+    private FourInARow game;
 
     @FXML
     private GridPane gameGrid;
@@ -19,6 +25,7 @@ public class GameController {
     private Button clearButton;
 
     public void initialize() {
+        game = new FourInARow(COLUMN_COUNT, ROW_COUNT);
         initializeContainer();
         initializeGrid();
         initializeButtons();
@@ -60,7 +67,29 @@ public class GameController {
     }
 
     private void onButtonPress(Button source) {
-        System.out.println(source.getText());
+        int currentTurn = this.game.getCurrentTurn();
+
+        Color currentColor =
+                currentTurn == FourInARow.PLAYER_ONE
+                        ? Color.BLUE
+                        : Color.RED;
+
+        int columnIndex = Integer.parseInt(source.getText()) - 1;
+        int rowIndex = this.game.play(columnIndex);
+        if (rowIndex == -1) return;
+
+        rowIndex = ROW_COUNT - 1 - rowIndex; // convert from top-left origin to bottom-left
+        placeDisc(currentTurn, columnIndex, rowIndex);
     }
 
+    private void placeDisc(int player, int colIndex, int rowIndex) {
+        Color currentColor =
+                player == FourInARow.PLAYER_ONE
+                        ? Color.BLUE
+                        : Color.RED;
+
+        // TODO: figure out how to center-align this circle to allow smaller radius
+        Circle circle = new Circle(SQUARE_CENTER, SQUARE_CENTER, DISC_RADIUS, currentColor);
+        this.gameGrid.add(circle, colIndex, rowIndex);
+    }
 }
